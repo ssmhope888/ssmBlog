35feb1e0e5775f13fc1f470c54fbb23a7769d828
shen jackson <shenjackson@192.168.0.182>
2024-05-13 13:11:51 +0800

    add host for vercel

--- GlobalContext.js
+++ GlobalContext.js
@@ -1,6 +1,6 @@
 
  // 根据环境变量修改host
- const prodHost = 'https://api.prod.com';
+ const prodHost = 'https://ssm-blog-okesuzg08-ssm1.vercel.app/';
  const devHost = 'http://localhost:3010/';
  
  // 全局可获取process.env.APP_ENV
