35291861a2c32407c95ee93443c5a5f2107a33fd
shen jackson <shenjackson@192.168.0.182>
2024-05-13 16:23:44 +0800

    add readme

--- GlobalContext.js
+++ GlobalContext.js
@@ -1,7 +1,7 @@
 
  // 根据环境变量修改host
  const prodHost = 'https://ssm-blog-okesuzg08-ssm1.vercel.app/';
- const devHost = 'https://ssm-blog-okesuzg08-ssm1.vercel.app/';
+ const devHost = 'http://localhost:3010/';
  
  // 全局可获取process.env.APP_ENV
  const appEnv = process.env.APP_ENV;
